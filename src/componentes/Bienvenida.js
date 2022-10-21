fe6ebeb0766904c8893df5c838e98472ec4e1ca9
T-Carri <bryan_narutito@hormail.com>
2022-10-21 18:04:56 -0500

    10212022

--- src/componentes/Bienvenida.js
+++ src/componentes/Bienvenida.js
@@ -1,13 +1,46 @@
-import { View, Text } from 'react-native'
+import { View, StyleSheet } from 'react-native'
 import React from 'react'
-
+import { Card, Text } from '@rneui/themed';
 export default function Bienvenida() {
   return (
     <View>
-      <Text>Bienvenida</Text>
+       <Card>
+          <Card.Title>SECMA</Card.Title>
+          <Card.Divider />
+ 
+ <Text style={styles.fonts} h2> Come on boy  </Text>
+
+
+
+
+
+
+        </Card>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+    },
+    fonts: {
+      marginBottom: 8,
+    },
+    user: {
+      flexDirection: 'row',
+      marginBottom: 6,
+    },
+    image: {
+      width: 30,
+      height: 30,
+      marginRight: 10,
+    },
+    name: {
+      fontSize: 16,
+      marginTop: 5,
+    },
+    });
   //TODO: ventana lateral con funciones de app segun tu sesion 
   //Bienvenido fulanito
   //Tu empresa
--- src/componentes/Checador.js
+++ src/componentes/Checador.js
@@ -1,12 +1,49 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { Card, Button } from '@rneui/themed';
+
 
 export default function Checador() {
   return (
     <View>
-      <Text>checador</Text>
+           <Card>
+          <Card.Title>Obra: mex6</Card.Title>
+          <Card.Divider />
+          <Button
+              title="QR"
+              icon={{
+                name: 'user',
+                type: 'font-awesome',
+                size: 15,
+                color: 'white',
+              }}
+              iconRight
+              iconContainerStyle={{ marginLeft: 10 }}
+              titleStyle={{ fontWeight: '700' }}
+              buttonStyle={{
+                backgroundColor: 'rgba(199, 43, 98, 1)',
+                borderColor: 'transparent',
+                borderWidth: 0,
+                borderRadius: 30,
+              }}
+              containerStyle={{
+                width: 200,
+                marginHorizontal: 50,
+                marginVertical: 10,
+              }}
+            />
+
+         </Card>
     </View>
   )
 }
 
+
 //TODO: JALAR DATOS DE API 
+
+//TODO: analizar "npm i qr-scanner"
+
+//coloque url: de asignacion 
+//hacer receptor de axios en interfaz de usuario checador "asigancion"
+//hacer disparador de axios en interfaz de usuario checador
+//colocar boton  con imagen de QR con acceso a QR
